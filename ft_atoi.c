366014a95cd7012cd9d6645fe7802c8db71fb917
Hexagon <anaschaouki011@gmail.com>
2022-10-26 22:22:12 +0100

    protected atoi function

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,10 +12,19 @@
 
 #include "libft.h"
 
+int	ft_isoverflow(int sign)
+{
+	if (sign == -1)
+		return (0);
+	else
+		return (-1);
+}
+
 int	ft_atoi(const char *str)
 {
 	size_t	i;
 	size_t	result;
+	size_t	prev;
 	int		sign;
 
 	i = 0;
@@ -31,7 +40,10 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		prev = result;
 		result = result * 10 + (str[i] - '0');
+		if ((result / 10) != prev)
+			return (ft_isoverflow(sign));
 		i++;
 	}
 	return (result * sign);
